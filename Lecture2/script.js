231a994de4694cf9563c480f03c9e696d4e1fa49
Smit <psmit9936@gmail.com>
2025-11-02 11:04:31 -0800

    Variables, Data type, Object

---
+++ Lecture2/script.js
@@ -0,0 +1,21 @@
+var a=5;
+
+{
+    let a=10;
+    console.log(a);
+}
+console.log(a);
+
+let b="Smit";
+let c=true;
+let d=null;
+let e=undefined;
+let f=3.66;
+
+console .log(typeof(b),typeof(c),typeof(d),typeof(e),typeof(f));
+
+let o={
+    "name": "Smit",
+    "job code": 5600
+}
+console.log(o);

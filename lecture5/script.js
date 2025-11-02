cb39bdd0f441befa9a0c112d9ea77f923015e55b
Smit <psmit9936@gmail.com>
2025-11-02 12:06:08 -0800

    Javascript Functions

---
+++ lecture5/script.js
@@ -0,0 +1,14 @@
+console.log("Javascript Functions");
+
+function sum (a, b){
+    return a+b;
+}
+
+let result=sum(5,6);
+console.log("The sum is: "+ result);
+
+const arrow=(x)=>{
+    console.log("Arrow Function", x);
+}
+
+arrow(37);

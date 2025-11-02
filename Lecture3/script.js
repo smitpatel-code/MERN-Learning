8572fbe3102ce87eaa29dba225541c689ac10fae
Smit <psmit9936@gmail.com>
2025-11-02 11:31:41 -0800

    Javascript conditional

---
+++ Lecture3/script.js
@@ -0,0 +1,15 @@
+console.log ("COnditional Tutorial");
+
+var age=19;
+ 
+if(age > 18){
+    console.log("You can drive");
+}
+else{
+    console.log("You cannot drive");
+}
+
+let x=9;
+let y=7;
+let c=(x>y)? (x-y) : (y-x);
+console.log(c);

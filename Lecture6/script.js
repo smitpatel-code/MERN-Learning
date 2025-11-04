52f6230209cf95b1d4488773e81fb36002641a9e
Smit <psmit9936@gmail.com>
2025-11-03 22:42:13 -0800

    Learned Strings

---
+++ Lecture6/script.js
@@ -0,0 +1,7 @@
+console.log ("Learning about Strings");
+let name="Smit";
+console.log(name[3]);
+let frndName="Om";
+
+console.log(`My name is ${name} and my friend's name is ${frndName}`);
+console.log(name.slice(1,3));
